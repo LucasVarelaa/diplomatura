f9fe946d2b6209220be3f8498d30a291f052ea3c
Lucas Varela <lucasvarela2004@gmail.com>
2024-03-15 19:22:58 -0300

    sitio

--- Sitio/front/src/components/layout/Header.js
+++ Sitio/front/src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = (props) => {
     return (
         <div className="holder">
-            <img src="img/infoDias.jpg" width="100" alt="Secretos de Familia" />
+            <img src="img/logo192.png" width="100" alt="Secretos de Familia" />
             <h1>Secretos de Familia</h1>
         </div>
     );
