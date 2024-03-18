724de339f31b85c5d58aee67dcc840367caa2bb5
Lucas Varela <lucasvarela2004@gmail.com>
2024-03-18 20:21:59 -0300

    sitio

--- Sitio/front/src/pages/NosotrosPage.js
+++ Sitio/front/src/pages/NosotrosPage.js
@@ -69,7 +69,6 @@ const NosotrosPage = (props) => {
                         <li>Se encarga de la logística y distribución de los pedidos.</li>
                         <li>Comprometido con la satisfacción del cliente en cada entrega.</li>
                     </ul>
-                    <h9>Hola</h9>
                 </div> 
             </div>
         </div>
