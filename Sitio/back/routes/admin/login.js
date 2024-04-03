fe7038b9a98f6d4ca939bbd3b14cfe87f2117810
LucasVarelaa <lucasvarela2004@gmail.com>
2024-04-02 21:53:59 -0300

    m5u4

--- Sitio/back/routes/admin/login.js
+++ Sitio/back/routes/admin/login.js
@@ -1,11 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  req.session.destroy();
   res.render('admin/login', {
     layout: 'admin/layout'
   });
 });
+
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  req.render('admin/login', {
+    layout: 'adming/layout'
+  });
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario; //capturando la info // lucas
+    var password = req.body.password; // 1234
+
+    var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
+    if (data != undefined) {
+
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
+
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 module.exports = router;
